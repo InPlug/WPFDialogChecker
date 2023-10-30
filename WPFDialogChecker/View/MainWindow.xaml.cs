6cc3aa8898b748175cb9293e6acf4b65c99c770d
Erik Nagel <erik@reallyhuman.net>
2023-10-30 15:35:58 +0100

    .Net 8 migration; version 8.0.0.0.

--- WPFDialogChecker/View/MainWindow.xaml.cs
+++ WPFDialogChecker/View/MainWindow.xaml.cs
@@ -19,11 +19,11 @@ namespace WPFDialogChecker.View
         public MainWindow()
         {
             /* Der nachfolgende Lock wurde erforderlich, da ansonsten (bei sehr großen Jobs mit sehr vielen Controls) folgender Fehler auftreten kann:
-             * Ietztes Ergebnis: Node334: System.NullReferenceException: Der Objektverweis wurde nicht auf eine Objektinstanz festgelegt.
+             * Letztes Ergebnis: Node334: System.NullReferenceException: Der Objektverweis wurde nicht auf eine Objektinstanz festgelegt.
              * bei System.lO.Packaging.PackagePart.CleanUpRequestedStreamsList()
              * bei System.IO.Packaging.PackagePart.GetStream(FileMode mode, FileAccess access)
              * bei System.Windows.Application.LoadComponent(Object component, Uri resourceLocator)
-             * bei WPFDialogChecker.View.MainWindow.lnitializeComponent()
+             * bei WPFDialogChecker.View.MainWindow.InitializeComponent()
              * bei WPFDialogChecker.View.MainWindow..ctor()
              * bei WPFDialogChecker.WPFDialogChecker.Run(Object checkerParameters, TreeParameters treeParameters, TreeEvent source)
              * bei LogicalTaskTree.CheckerShell.runlt(Object checkerParameters, TreeParameters treeParameters, TreeEvent source) in C:\Users\micro\Documents\private4\WPF\Vishnu_Root\VishnuHome\Vishnu\LogicalTaskTree\CheckerShell.cs:Zeile 444.
--- WPFDialogChecker/WPFDialogChecker.cs
+++ WPFDialogChecker/WPFDialogChecker.cs
@@ -36,7 +36,7 @@ namespace WPFDialogChecker
         }
 
         /// <summary>
-        /// Kann aufgerufen werden, wenn sich der Verarbeitungs-Fortschritt
+        /// Kann aufgerufen werden, wenn sich der Verarbeitungsfortschritt
         /// des Checkers geändert hat, muss aber zumindest aber einmal zum
         /// Schluss der Verarbeitung aufgerufen werden.
         /// </summary>
